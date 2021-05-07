6725c4badd45600dd32d58b419211881616a091c
Alan Razo Peña <alanrazop@MacBook-Air-de-Alan.local>
2021-05-06 23:30:58 -0500

    Funcion de padding

--- padding.py
+++ padding.py
@@ -4,3 +4,18 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 
+"Dimension de la imagen: 920x1900
+"
+
+"Funcion que convierte las matrices con numpy"
+def padding (mat, rows, cols):
+    mat = np.matrix(mat)
+    "Más renglones y columnas que la matriz original"
+    m = math.floor((rows - np.shape(mat)[0]) / 2)
+    n = math.floor((cols - np.shape(mat)[1]) / 2)
+    
+    "Matriz final"
+    final = np.zeros((rows, cols))
+    
+    
+    
